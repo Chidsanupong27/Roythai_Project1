cbb9f3fcf94d11e9141263be3f7f72b0862ef174
Chidsanupong27 <Chidsan7@gmail.com>
2025-10-22 01:58:56 +0700

    ep1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <h1> Easy react</h1>;
+import React from 'react'
+import Title from './components/Title'
+import Book from './components/Book'
+
+const App = () => {
+
+
+  return (
+    <>
+      <Title/>
+      <Book/>
+    </>
+  )
 }
 
-export default App;
+export default App
